44f8f83bebf0857ebea1906418a3a1dfeaaa5d12
minz666 <144245436+minz666@users.noreply.github.com>
2024-02-04 15:47:00 +0700

    facebook.sh

--- facebook.sh
+++ facebook.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 VBR="1500k"
 FPS="30"
@@ -6,7 +6,7 @@ QUAL="veryfast"
 RTMP_URL="rtmps"
 RTMP_KEY="rtmp key"
 VIDEO_SOURCE="8888.gif"
-AUDIO_SOURCE="musik.mp3"
+AUDIO_SOURCE="mp3/your.mp3"
 AUDIO_ENCODER="aac"
 
 ffmpeg \
@@ -18,5 +18,5 @@ ffmpeg \
  -i "$AUDIO_SOURCE" \
  -c:v libx264 -preset $QUAL -r $FPS -g $FPS -b:v $VBR \
  -c:a $AUDIO_ENCODER -threads 6 -ar 44100 -b:a 128k -bufsize 512k -pix_fmt yuv420p -s 1280x720 \
- -fflags +shortest -max_interleave_delta 50000 \
- -f flv $RTMP_URL/$RTMP_KEY
+ -fflags +shortest -max_interleave_delta 0 \
+ -f flv "$RTMP_URL/$RTMP_KEY"
