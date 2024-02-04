09feb620d516c8af9f71664e76545153f7df6a87
minz666 <144245436+minz666@users.noreply.github.com>
2024-02-04 18:38:44 +0700

    facebook.sh

--- facebook.sh
+++ facebook.sh
@@ -3,10 +3,10 @@
 VBR="1500k"
 FPS="30"
 QUAL="veryfast"
-RTMP_URL="rtmps"
+RTMP_URL="rtmps://live-api-s.facebook.com:443/rtmp/"
 RTMP_KEY="rtmp key"
 VIDEO_SOURCE="8888.gif"
-AUDIO_SOURCE="mp3/your.mp3"
+AUDIO_SOURCE="https://stream.zeno.fm/0r0xa792kwzuv"
 AUDIO_ENCODER="aac"
 
 ffmpeg \
@@ -18,5 +18,5 @@ ffmpeg \
  -i "$AUDIO_SOURCE" \
  -c:v libx264 -preset $QUAL -r $FPS -g $FPS -b:v $VBR \
  -c:a $AUDIO_ENCODER -threads 6 -ar 44100 -b:a 128k -bufsize 512k -pix_fmt yuv420p -s 1280x720 \
- -fflags +shortest -max_interleave_delta 0 \
+ -fflags -shortest -max_interleave_delta 0 \
  -f flv "$RTMP_URL/$RTMP_KEY"
